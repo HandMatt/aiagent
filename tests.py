2e4a511e9d0ef8a6a5a57c7abe423bd2e6552001
HandMatt <74497505+HandMatt@users.noreply.github.com>
2025-09-28 21:49:31 +0100

    Add file reading with path validation and character limits
    - implements get_file_content()
    - add character limit 10,000
    - prevent directory traversal attacks
    - add tests

--- tests.py
+++ tests.py
@@ -1,31 +1,19 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print(f"Result for current directory:")
+    result = get_file_content("calculator", "main.py")
     print(result)
-    print("")
 
-    result = get_files_info("calculator", "pkg")
-    print(f"Result for 'pkg' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
     print(result)
-    print("")
 
-    result = get_files_info("calculator", "/bin")
-    print(f"Result for '/bin' directory:")
+    result = get_file_content("calculator", "/bin/cat")
     print(result)
-    print("")
 
-    result = get_files_info("calculator", "../")
-    print(f"Result for '../' directory:")
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
     print(result)
-    print("")
-    print(
-        f"Result for '/bin' directory:\n{get_files_info("calculator", "/bin")}")
-    print(
-        f"Result for '../' directory:\n{get_files_info("calculator", "../")}")
-
 
 if __name__ == "__main__":
     test()
+
